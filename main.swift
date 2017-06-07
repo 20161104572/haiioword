ca3141afec3e0d9d9fbdcf44460dc0675f9b8951
20161104572 <2606612104>
2017-06-07 10:44:26 +0800

    hello
    jdkwjd

---
+++ main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  haiioword
+//
+//  Created by 20161104572 on 17/6/7.
+//  Copyright © 2017年 20161104572. All rights reserved.
+//
+#include <stdio.h>
+int main()
+{
+    printf("helloword");
+    return 0;
+}
